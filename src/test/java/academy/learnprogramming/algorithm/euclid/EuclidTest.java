c6e926fbd84c19bc38a3b55f7f89feec97ef5fc6
Lucas Lodi Kreusberg <gitkreusberg@gmail.com>
2024-07-08 19:28:02 -0300

    implement tests for Euclid Algorithm versions

---
+++ src/test/java/academy/learnprogramming/algorithm/euclid/EuclidTest.java
@@ -0,0 +1,37 @@
+package academy.learnprogramming.algorithm.euclid;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class EuclidTest {
+
+    Euclid e = null;
+
+    @Before
+    public void init() {
+        e = new Euclid();
+    }
+
+    @Test
+    public void gcdTest() {
+        Assert.assertEquals(2, e.gcd(10, 2));
+        Assert.assertEquals(2, e.gcd(22, 6));
+        System.out.println(e.gcd(79, 32));
+    }
+
+    @Test
+    public void gcd2Test() {
+        Euclid e = new Euclid();
+        Assert.assertEquals(2, e.gcd2(10, 2));
+        Assert.assertEquals(2, e.gcd2(22, 6));
+    }
+
+    @Test
+    public void gcdVersusGcd2Test() {
+        Assert.assertEquals(e.gcd(10,2), e.gcd2(10, 2));
+        Assert.assertEquals(e.gcd(22,6), e.gcd2(22, 6));
+        Assert.assertEquals(e.gcd(182,74), e.gcd2(182, 74));
+
+    }
+}
