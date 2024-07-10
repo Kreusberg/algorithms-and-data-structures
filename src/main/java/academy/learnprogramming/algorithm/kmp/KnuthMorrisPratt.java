75a20b1ecf98e319c37a8887943f3f774130ce51
Lucas Lodi Kreusberg <gitkreusberg@gmail.com>
2024-07-09 22:26:27 -0300

    implement Search Knuth Morris Pratt algorithm

--- src/main/java/academy/learnprogramming/algorithm/kmp/KnuthMorrisPratt.java
+++ src/main/java/academy/learnprogramming/algorithm/kmp/KnuthMorrisPratt.java
@@ -1,10 +1,37 @@
 package academy.learnprogramming.algorithm.kmp;
 
-/**
- * Computes the table of the longest suffix and prefix on the pattern.
- */
 public class KnuthMorrisPratt {
 
+    /**
+     * Retuns the index for the first match of the pattern.
+     * Will return -1 in case where it does not match.
+     * @param array
+     * @param pattern
+     * @return
+     */
+    public int search(char[] array, char[] pattern) {
+        int[] lsp = computeLSPTable(pattern);
+        int j = 0;
+
+        for (int i = 0; i < array.length; i++) {
+            while (j > 0 && array[i] != pattern[j]) {
+                j = lsp[j - 1];
+            }
+
+            if (array[i] == pattern[j]) {
+                j++;
+                if (j == pattern.length) {
+                    return (i - (j-1));
+                }
+            }
+        }
+
+        return -1;
+    }
+
+    /**
+     * Computes the table of the longest suffix and prefix on the pattern.
+     */
     // LSP stands for Longest Suffix Prefix
     public int[] computeLSPTable(char[] pattern) {
         int[] lsp = new int[pattern.length];
